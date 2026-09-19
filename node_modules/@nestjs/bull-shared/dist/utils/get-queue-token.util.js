1e42269a08c105bfbc18a2656f4df2d80b954dc3
Ismadbek <ismadbek@Ismadbeks-MacBook-Air.local>
2026-09-19 16:05:41 +0500

    Node changed

--- node_modules/@nestjs/bull-shared/dist/utils/get-queue-token.util.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getQueueToken = getQueueToken;
-function getQueueToken(name) {
-    return name ? `BullQueue_${name}` : 'BullQueue_default';
-}
